c01b6d11fa3d187b70abb703135ad31745a8c230
mykealw <mykeal.wheeler@gmail.com>
2022-05-27 13:37:19 -0600

    tables set up build paths now

--- AllSpice/dbSetup.sql
+++ AllSpice/dbSetup.sql
@@ -1,8 +1,42 @@
 CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS favorites(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    accountId VARCHAR(255) NOT NULL,
+    recipeId INT NOT NULL,
+    FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS ingredients(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity VARCHAR(255) NOT NULL,
+    recipeId INT NOT NULL,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS recipes(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    picture TEXT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    subtitle VARCHAR(255) DEFAULT "",
+    category VARCHAR(255) NOT NULL,
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE IF NOT EXISTS steps(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    position INT NOT NULL,
+    body TEXT NOT NULL,
+    recipeId INT NOT NULL,
+    FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+) default charset utf8;
